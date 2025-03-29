fa3f1adc0b1ae3a7d423717035222235662890a4
Alice Campos <allicefcampos@gmail.com>
2025-03-29 12:01:11 -0300

    creating components

---
+++ src/components/tarefa.jsx
@@ -0,0 +1,13 @@
+function Tarefa() {
+    return (
+      <div>
+        <h2>Nome da tarefa:</h2>
+        <p>descrição detalhada da tarefa</p>
+        <button>Excluir</button>
+        <button>Editar</button>
+      </div>
+    );
+  }
+  
+  export default Tarefa;
+  
